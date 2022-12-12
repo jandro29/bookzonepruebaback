8d405a3d23475e50b5c4edc57cac2381e363cea8
jandro29 <ascolanr@gmail.com>
2022-12-12 01:22:50 -0500

    first commit

--- src/app/views/opciones/clientes/cliente.service.ts
+++ src/app/views/opciones/clientes/cliente.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Cliente } from './cliente';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private url:string="/*urldelspring*/";
+
+  constructor(private http:HttpClient) { }
+
+  //obtener todos los clientes
+
+  getAll():Observable<Cliente[]>{
+    return this.http.get<Cliente[]>(this.url);
+  }
+
+  //crear cliente
+  create(cliente:Cliente):Observable<Cliente>{
+    return this.http.post<Cliente>(this.url, cliente);
+  }
+
+  //obtener un cliente
+  get(id:number):Observable<Cliente[]>{
+    return this.http.get<Cliente[]>(this.url+'/'+id);
+  }
+
+  //actualizar cliente
+
+  update(cliente:Cliente):Observable<Cliente>{
+    return this.http.put<Cliente>(this.url, cliente);
+  }
+
+  //eliminar cliente
+
+  delete(id:number):Observable<Cliente[]>{
+    return this.http.delete<Cliente[]>(this.url+'/'+id);
+  }
 }
